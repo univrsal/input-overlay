1f925691a31c33cb7c69977d466d6016df80b45f
Alex <univrsal@protonmail.ch>
2018-07-14 19:47:44 +0200

    Constants

---
+++ util/layout_constants.hpp
@@ -0,0 +1,18 @@
+/**
+ * Created by univrsal on 06.07.2018.
+ * This file is part of input-overlay which is licensed
+ * under the MOZILLA PUBLIC LICENSE 2.0 - mozilla.org/en-US/MPL/2.0/
+ * github.com/univrsal/input-overlay
+ */
+#pragma once
+
+#define CFG_X_POS	"_x_pos"
+#define CFG_Y_POS	"_y_pos"
+#define CFG_U		"_u"
+#define CFG_V		"_v"
+#define CFG_TYPE	"_type"
+#define CFG_KEY_CODE	"_code"
+#define CFG_WIDTH	"_width"
+#define CFG_HEIGHT	"_height"
+#define CFG_FIRST_ID	"first_element"
+#define CFG_NEXT_ID	"_next"
