83717f72e4f3689851c5103c829ffad4cfe37735
Alex <univrsal@protonmail.ch>
2018-09-10 16:03:57 +0200

    Updated constants

--- io-obs/util/layout_constants.hpp
+++ io-obs/util/layout_constants.hpp
@@ -6,35 +6,71 @@
  */
 #pragma once
 
-#define CFG_DEFAULT_WIDTH	"default_width"
-#define CFG_DEFAULT_HEIGHT	"default_height"
-#define CFG_H_SPACE		"space_h"
-#define CFG_V_SPACE		"space_v"
+#define CFG_INNER_BORDER    3
+#define CFG_OUTTER_BORDER   1
 
-#define CFG_TOTAL_WIDTH	"overlay_width"
-#define CFG_TOTAL_HEIGHT "overlay_height"
+#define CFG_DEFAULT_WIDTH   "default_width"
+#define CFG_DEFAULT_HEIGHT  "default_height"
+#define CFG_H_SPACE         "space_h"
+#define CFG_V_SPACE         "space_v"
 
-#define CFG_X_POS	"_x_pos"
-#define CFG_Y_POS	"_y_pos"
-#define CFG_U		"_u"
-#define CFG_V		"_v"
-#define CFG_TYPE	"_type"
-#define CFG_KEY_CODE	"_code"
-#define CFG_WIDTH	"_width"
-#define CFG_HEIGHT	"_height"
-#define CFG_Z_LEVEL	"_z_level"
-#define CFG_FIRST_ID	"first_element"
-#define CFG_NEXT_ID	"_next"
+#define CFG_TOTAL_WIDTH     "overlay_width"
+#define CFG_TOTAL_HEIGHT    "overlay_height"
+
+#define CFG_X_POS           "_x_pos"
+#define CFG_Y_POS           "_y_pos"
+#define CFG_U               "_u"
+#define CFG_V               "_v"
+#define CFG_TYPE            "_type"
+#define CFG_KEY_CODE        "_code"
+#define CFG_WIDTH           "_width"
+#define CFG_HEIGHT          "_height"
+#define CFG_Z_LEVEL         "_z_level"
+#define CFG_FIRST_ID        "first_element"
+#define CFG_NEXT_ID         "_next"
+#define CFG_SIDE            "_side"
+#define CFG_STICK_RADIUS    "_stick_radius"
+#define CFG_MOUSE_RADIUS    "_mouse_radius"
+#define CFG_MOUSE_TYPE      "_mouse_type"
+#define CFG_DIRECTION       "_direction"
+#define CFG_TRIGGER_MODE    "_trigger_mode"
+
+/* Misc */
+#define AXIS_MAX_AMPLITUDE 32767
+#define STICK_DEAD_ZONE     100
+
+enum MouseMovementType
+{
+    DOT,
+    ARROW
+};
+
+enum ElementSide
+{
+    SIDE_LEFT,
+    SIDE_RIGHT
+};
+
+enum TriggerDirection
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
 
 enum ElementType
 {
-	INVALID = -1,
-	TEXTURE,
-	BUTTON,
-	MOUSE_SCROLLWHEEL, /* MOUSE_WHEEL was taken :( */
-	MOUSE_MOVEMENT,
-	ANALOG_STICK,
-	TRIGGER,
-	TEXT,
-	DPAD_STICK,
+    INVALID = -1,
+    TEXTURE,
+    BUTTON,
+    MOUSE_SCROLLWHEEL,
+    /* MOUSE_WHEEL was taken :( */
+    MOUSE_MOVEMENT,
+    ANALOG_STICK,
+    TRIGGER,
+    GAMEPAD_ID,
+    /* Shows game pad number 1 through 4 */
+    TEXT,
+    DPAD_STICK,
 };
