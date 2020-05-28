5af39ea77f1612364a3a27c7e5228f48f03c96b7
univrsal <univrsal@protonmail.ch>
2020-05-28 08:06:57 +0200

    fix compile error for io-cct

--- io-cct/src/util/util.hpp
+++ io-cct/src/util/util.hpp
@@ -26,7 +26,6 @@
 #define TO_PAD_MASK(a) (a | SDL_PAD_MASK)
 #define TO_MOUSE_MASK(a) (a | SDL_MOUSE_MASK)
 
-==== BASE ====
 #define SDL_PAD_MASK 0x00FF0000   /* SDL uses 32bit integer for */
 #define SDL_MOUSE_MASK 0x00FE0000 /* codes so there's plenty of space */
 #define TO_PAD_MASK(a) (a | SDL_PAD_MASK)
@@ -34,41 +33,40 @@
 
 #define UTIL_MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define UTIL_MIN(a, b) (((a) < (b)) ? (a) : (b))
-==== BASE ====
-	#define UTIL_CLAMP(lower, x, upper)(UTIL_MIN(upper, UTIL_MAX(x, lower)))
+#define UTIL_CLAMP(lower, x, upper)(UTIL_MIN(upper, UTIL_MAX(x, lower)))
 
-		SDL_bool
-		util_move_element(int *x, int *y, SDL_Keycode key);
+SDL_bool
+util_move_element(int *x, int *y, SDL_Keycode key);
 
 using json = nlohmann::json;
 
 class timer {
 public:
-	Timer() { start(); };
+    Timer() { start(); };
 
-	void start()
-	{
-		m_start_ticks = SDL_GetTicks();
-		m_end_ticks = 0;
-		m_started = true;
-	}
+    void start()
+    {
+        m_start_ticks = SDL_GetTicks();
+        m_end_ticks = 0;
+        m_started = true;
+    }
 
-	void stop()
-	{
-		m_end_ticks = SDL_GetTicks();
-		m_started = true;
-	}
+    void stop()
+    {
+        m_end_ticks = SDL_GetTicks();
+        m_started = true;
+    }
 
-	bool started() const { return m_started; }
+    bool started() const { return m_started; }
 
-	uint32_t get_delta() const { return m_end_ticks - m_start_ticks; }
+    uint32_t get_delta() const { return m_end_ticks - m_start_ticks; }
 
-	uint32_t get_time() const { return SDL_GetTicks() - m_start_ticks; }
+    uint32_t get_time() const { return SDL_GetTicks() - m_start_ticks; }
 
 private:
-	uint32_t m_start_ticks = 0;
-	uint32_t m_end_ticks = 0;
-	bool m_started = false;
+    uint32_t m_start_ticks = 0;
+    uint32_t m_end_ticks = 0;
+    bool m_started = false;
 };
 
 namespace util {
