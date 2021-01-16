e0b12cb2cf98c8569ed03aa123b60ef340ecb8d3
univrsal <univrsal@protonmail.ch>
2021-01-16 21:40:32 +0100

    update formatting

--- format.sh
+++ format.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-grep -rl 'Copyright 2019' ./projects | xargs sed -i 's/Copyright 2019/Copyright 2020/g'
+grep -rl 'Copyright 2020' ./projects | xargs sed -i 's/Copyright 2019/Copyright 2021/g'
+grep -rl 'uni@vrsal.cf' ./projects | xargs sed -i 's/uni@vrsal.cf/uni@vrsal.de/g'
 find ./deps/common -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./projects -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
+find ./docs  -type f \( ! -iname jquery* \) -iname *.js -o -iname *.html | xargs clang-format -i -verbose
