4db7a2866b11e44100cd81c8337dbfb1861047d9
univrsal <univrsal@protonmail.ch>
2021-01-24 00:19:24 +0100

    update format script

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep -rl 'Copyright 2020' ./projects | xargs sed -i 's/Copyright 2019/Copyright 2021/g'
+grep -rl 'Copyright 2020' ./projects | xargs sed -i 's/Copyright 2020/Copyright 2021/g'
 grep -rl 'uni@vrsal.cf' ./projects | xargs sed -i 's/uni@vrsal.cf/uni@vrsal.de/g'
 find ./deps/common -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./projects -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
