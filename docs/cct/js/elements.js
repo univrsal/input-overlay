bc4caa816b630cbad2229a1ad92eaed02e13e9a7
univrsal <uni@vrsal.de>
2021-03-23 22:05:22 +0100

    Read/Write data from and to gui

--- docs/cct/js/elements.js
+++ docs/cct/js/elements.js
@@ -30,28 +30,37 @@ let element_types = {
     MOUSE_MOVEMENT: 9
 };
 
-let constants = {texture_space: 3};
+let constants = { texture_space: 3 };
 
 let element_map = new Map();
 
 class element {
     constructor(json) { this.data = json; }
 
-    draw(painter) {}
-    on_button_input(vc, state) {}
+    draw(painter) { }
+    on_button_input(vc, state) { }
 
-    scaled_dim(cs)
-    {
+    scaled_dim(cs) {
         return new r4(cs.origin.x - cs.offset.x + this.data.pos[0] * cs.scale,
-                      cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
-                      this.data.mapping[3] * cs.scale);
+            cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
+            this.data.mapping[3] * cs.scale);
     }
-    read_data_from_gui() {}
+    
+    read_data_from_gui()
+    {
+        this.data.id = $("#editor-element-id").val();
+        this.data.z_level = parseInt($("#editor-element-layer").val());
+    }
+
+    write_data_to_gui()
+    {
+        $("#editor-element-id").val(this.data.id);
+        $("#editor-element-layer").val(this.data.z_level);
+    }
 
     dim() { return new r4(this.data.pos[0], this.data.pos[1], this.data.mapping[2], this.data.mapping[3]); }
 
-    set_dim(pos, uvwh)
-    {
+    set_dim(pos, uvwh) {
         this.data.pos[0] = pos.x;
         this.data.pos[1] = pos.y;
         this.data.mapping[0] = uvwh.x;
@@ -69,8 +78,7 @@ class element {
     h() { return this.data.mapping[3]; }
     set_id(id) { this.data.id = id; }
 
-    set_pos(x, y)
-    {
+    set_pos(x, y) {
         this.data.pos[0] = x;
         this.data.pos[1] = y;
     }
@@ -81,33 +89,48 @@ class element {
 
 class texture extends element {
 
-    draw(painter)
-    {
+    draw(painter) {
         // default draw handling
         let cs = painter.cs();
         painter.image_crop(atlas, cs.origin.x - cs.offset.x + this.data.pos[0] * cs.scale,
-                           cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
-                           this.data.mapping[3] * cs.scale, this.data.mapping[0], this.data.mapping[1],
-                           this.data.mapping[2], this.data.mapping[3]);
+            cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
+            this.data.mapping[3] * cs.scale, this.data.mapping[0], this.data.mapping[1],
+            this.data.mapping[2], this.data.mapping[3]);
+    }
+
+    read_data_from_gui() {
+        super.read_data_from_gui();
+        this.data.mapping = [
+            parseInt($("#editor-element-u").val()),
+            parseInt($("#editor-element-v").val()),
+            parseInt($("#editor-element-w").val()),
+            parseInt($("#editor-element-h").val())
+        ];
+    }
+
+    write_data_to_gui() {
+        super.write_data_to_gui();
+        $("#editor-element-u").val(this.data.mapping[0]);
+        $("#editor-element-v").val(this.data.mapping[1]);
+        $("#editor-element-w").val(this.data.mapping[2]);
+        $("#editor-element-h").val(this.data.mapping[3]);
     }
 };
 
 class button extends texture {
-    constructor(json)
-    {
+    constructor(json) {
         super(json);
         this.pressed = false;
     }
 
-    draw(painter)
-    {
+    draw(painter) {
         if (this.pressed) {
             let cs = painter.cs();
             painter.image_crop(atlas, cs.origin.x - cs.offset.x + this.data.pos[0] * cs.scale,
-                               cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
-                               this.data.mapping[3] * cs.scale, this.data.mapping[0],
-                               this.data.mapping[1] + constants.texture_space + this.data.mapping[3],
-                               this.data.mapping[2], this.data.mapping[3]);
+                cs.origin.y - cs.offset.y + this.data.pos[1] * cs.scale, this.data.mapping[2] * cs.scale,
+                this.data.mapping[3] * cs.scale, this.data.mapping[0],
+                this.data.mapping[1] + constants.texture_space + this.data.mapping[3],
+                this.data.mapping[2], this.data.mapping[3]);
         } else {
             super.draw(painter, cs);
         }
@@ -117,23 +140,26 @@ class button extends texture {
 class keyboard_button extends button {
     constructor(json) { super(json); }
 
-    on_button_input(vc, state)
-    {
+    on_button_input(vc, state) {
         if (vc == this.data.code)
             this.pressed = state;
     }
 
-    read_data_from_gui()
-    {
+    read_data_from_gui() {
+        super.read_data_from_gui();
         this.data.code = parseInt($('#editor-element-keycode').val(), 16);
+    }
+
+    write_data_to_gui() {
+        super.write_data_to_gui();
+        $('#editor-element-keycode').val(this.data.code);
     }
 };
 
 element_map.set(element_types.KEYBOARD_KEY, json => { return new keyboard_button(json); });
 element_map.set(element_types.TEXTURE, json => { return new texture(json); });
 
-function create_element(json)
-{
+function create_element(json) {
     if (element_map.has(json.type))
         return element_map.get(json.type)(json);
     return null;
