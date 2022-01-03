2b99e3506459ed4ddd0056eb7d2a6124fbcce4c1
univrsal <uni@vrsal.de>
2022-01-03 12:50:36 +0100

    Misc: Update copyright header

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -rl 'Copyright 2020' ./projects | xargs sed -i 's/Copyright 2020/Copyright 2021/g'
-grep -rl 'uni@vrsal.cf' ./projects | xargs sed -i 's/uni@vrsal.cf/uni@vrsal.de/g'
+grep -rl 'Copyright 2021' ./projects | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
+grep -rl 'uni@vrsal.de' ./projects | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
 find ./deps/common -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./projects -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./docs -type f ! -iname "jquery-3.5.1.min.js" -name "*.js" -o -name "*.html" ! -path "css" | xargs clang-format -i -verbose
