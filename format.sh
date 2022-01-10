07447751e78c3b81e74aa91dd3a02728c266b76e
univrsal <uni@vrsal.de>
2022-01-10 14:47:22 +0100

    Misc: Update format.sh

--- format.sh
+++ format.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-grep -rl 'Copyright 2021' ./projects | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
-grep -rl 'uni@vrsal.de' ./projects | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
+#grep -rl 'Copyright 2021' ./projects | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
+#grep -rl 'uni@vrsal.de' ./projects | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
+#grep -rl 'Copyright 2021' ./docs | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
+#grep -rl 'uni@vrsal.de' ./docs | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
+
 find ./deps/common -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./projects -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./docs -type f ! -iname "jquery-3.5.1.min.js" -name "*.js" -o -name "*.html" ! -path "css" | xargs clang-format -i -verbose
