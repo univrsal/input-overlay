106bec9aafb5242a3c1cdb46a71160ac7138296b
univrsal <uni@vrsal.xyz>
2022-08-16 19:57:50 +0200

    Misc: Update format script

--- format.sh
+++ format.sh
@@ -5,6 +5,7 @@
 #grep -rl 'uni@vrsal.de' ./docs | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
 
 find ./deps/common -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
-find ./projects -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
+find ./client -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
+find ./src -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
 find ./docs -type f ! -iname "jquery-3.5.1.min.js" -name "*.js" -o -name "*.html" ! -path "css" | xargs clang-format -i -verbose
 
